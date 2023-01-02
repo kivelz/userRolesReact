e51728bc13a55767f29e5850b0c500216b567e9b
kelvin tay <hakwee.soh@zionext.com>
2023-01-02 16:05:09 +0800

    add contact function

--- src/app/pages/AddUserContact.tsx
+++ src/app/pages/AddUserContact.tsx
@@ -18,11 +18,16 @@ let contactInfo = [
 
 const AddUsefulContact: React.FunctionComponent<IAddUserContact> = (props) => {
     const [userContact, setAddUserContact] = React.useState(contactInfo)
+    const handleClick = () => {
+
+        setAddUserContact((current: any) => [...current, { name: 'dsad', telNo: '625465847' }]);
+    };
 
     return (
-        <div style={{ flex: 1, }}>
+        <div style={{ flex: 1 }}>
+
             {userContact.map((item, index) => (
-                <div style={{ paddingTop: 24, paddingBottom: 24 }}>
+                <div style={{ paddingTop: 24, paddingBottom: 24, flex: 1 }}>
                     <div>
                         {`Useful Contact` + ' ' + (index)}
                     </div>
@@ -31,18 +36,21 @@ const AddUsefulContact: React.FunctionComponent<IAddUserContact> = (props) => {
                             <div className='textBoxTitleSpace'>
                                 <span className='textBoxLabel' style={{ color: '#6A717B' }}>Name:</span>
                             </div>
-                            <TextField id="outlined-basic" variant="outlined" fullWidth size='small' style={{ paddingRight: 24 }} placeholder='+65 2544 8741' />
+                            <TextField id="outlined-basic" variant="outlined" fullWidth size='small' style={{ paddingRight: 24 }} placeholder={item.name ? item.name : 'eg. Police Hotline'} />
                         </div>
                         <div style={{ flex: 1 }}>
                             <div className='textBoxTitleSpace'>
                                 <span className='textBoxLabel' style={{ color: '#6A717B' }}>Telephone Number)</span>
                             </div>
-                            <TextField id="outlined-basic" variant="outlined" fullWidth size='small' style={{}} />
+                            <TextField id="outlined-basic" variant="outlined" fullWidth size='small' style={{}} placeholder={item.telNo ? item.telNo : 'eg. 999'} />
                         </div>
                     </div>
                 </div>
             ))}
-            <Button onClick={() => alert('in progress')} >Add Contact</Button>
+            <div style={{ display: 'flex' }}>
+                <Button style={{ alignSelf: 'flex-end', alignItems: 'fkex-end', justifyContent: 'flex-end', marginLeft: 'auto' }} onClick={() => handleClick()} >Add Contact</Button>
+            </div>
+
         </div>
     )
 }
